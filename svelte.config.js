0b7323b4129df6ad7e4d154291a7998c4c986edc
philrwebb <prwebb@optusnet.com.au>
2022-03-07 12:04:54 +0800

    vite config in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,23 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    "extensions": [".svelte", ...mdsvexConfig.extensions],
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-    kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      server: {
+        fs: {
+          // Allow serving files from one level up to the project root
+          allow: ['/Users/philipwebb/Development'],
+        },
+      },
+    },
+  },
 
-    preprocess: [mdsvex(mdsvexConfig)]
-};
+  preprocess: [mdsvex(mdsvexConfig)],
+}
 
-export default config;
+export default config
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-export default defineConfig({
-    server: {
-      fs: {
-        // Allow serving files from one level up to the project root
-        allow: ['/Users/philipwebb/Development']
-      }
-    }
-  })
